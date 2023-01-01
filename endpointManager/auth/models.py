23f6a577f064bdcb8308582656e25a6c2be782c3
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-02 02:54:02 +0330

    feat: complete user model

--- endpointManager/auth/models.py
+++ endpointManager/auth/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+
+class User(AbstractUser):
+  
+    name = models.CharField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = []
+    class Meta:
+        # managed = False
+        db_table = 'User'
+ 
+ 
