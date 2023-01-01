5052c8056c503ee9a1bac227057b07836a981703
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-02 02:59:56 +0330

    feat: register user to admin

--- endpointManager/auth/admin.py
+++ endpointManager/auth/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ( 'name','password','username' ,'created_at','updated_at')
+    list_filter = ( 'name','password','username' ,'created_at','updated_at')
+    fieldsets = (
+        ('infos', {'fields': ('name','password','username' )}),
+        ('Permissions', {
+         'fields': ('is_staff', 'is_active', 'user_permissions')}),
+    )
+    
+    search_fields = ('name','username' ,'created_at','updated_at')
+    ordering = ('name','username' ,'created_at','updated_at')
+      
