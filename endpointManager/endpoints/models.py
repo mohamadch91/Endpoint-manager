9b60fa163f2d32b9a060a037ee7cd8afab9066e0
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-02 03:15:28 +0330

    feat: create URL model

--- endpointManager/endpoints/models.py
+++ endpointManager/endpoints/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+from auth.models import User
+
+class Url(models.Model):
+    url = models.CharField(max_length=200, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    fail_limit = models.IntegerField(default=0)
+    success_count = models.IntegerField(default=0)
+    fail_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'Url'
