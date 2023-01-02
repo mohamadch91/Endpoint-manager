4aa191462c8c7eaf7813e4490f372e6353892aa7
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-02 03:38:00 +0330

    feat: warnings model add

--- endpointManager/warning/models.py
+++ endpointManager/warning/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from endpoints.models import Url
+
+class Warning(models.Model):
+
+    id=models.AutoField(primary_key=True)
+    url = models.ForeignKey(Url, on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'Warning'
