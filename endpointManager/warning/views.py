4842e8e52bd7c1531044db5b47afb43ba6d4c183
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-02 03:45:10 +0330

    feat: get warnings View

--- endpointManager/warning/views.py
+++ endpointManager/warning/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .serializers import GetWarningSerializer, WarningSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Warning
+from auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+import copy
+class GetWarningView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = GetWarningSerializer
+    def get_queryset(self):
+        warnings=Warning.objects.filter(url=self.request.url)
+        serializers=WarningSerializer(warnings,many=True)
+        serializers_copy=copy.deepcopy(serializers.data)
+        for i in serializers_copy:
+            del i['url']
+        return Response(serializers_copy,status=status.HTTP_200_OK)
+        
