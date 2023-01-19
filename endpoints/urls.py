35ba1640ec4dfbe951d5ae24c4454b18a02d7f40
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-19 12:03:23 +0330

    feat: update URL stat for one day

--- endpoints/urls.py
+++ endpoints/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('create/', UrlCreateView.as_view(), name='url_create'),
     path('user_urls/', UserUrlView.as_view(), name='user_urls'),
     path('url_stats/<int:pk>/', UrlStatsView.as_view(), name='url_stats'),
+    path('<int:pk>', UrlStatsView.as_view(), name='url_stats'),
+    
     
 
 
--- endpoints/views.py
+++ endpoints/views.py
@@ -1,16 +1,16 @@
 
 # Create your views here.
-from .serializers import UrlSerializer
-from rest_framework.permissions import AllowAny
+from .serializers import UrlSerializer,RequestSerializer
 
-from .models import Url
+from .models import Url,Request
 from authen.models import User
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated ,AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+import datetime
 
 class UrlCreateView(generics.CreateAPIView):
     permission_classes = (IsAuthenticated,)
@@ -44,14 +44,24 @@ class UrlStatsView(generics.RetrieveAPIView):
     serializer_class = UrlSerializer
     def get_queryset(self):
         url=get_object_or_404(Url,pk=self.kwargs['pk'])
-        response={
-            'success_count':url.success_count,
-            'fail_count':url.fail_count,
-            'fail_limit':url.fail_limit,
-            'total_count':url.success_count+url.fail_count
-        }
-        return Response(response,status=status.HTTP_200_OK)
-
+        if (url.user==self.request.user):
+            yesterday=datetime.datetime.now()-datetime.timedelta(days=1)
+            requests=Request.objects.filter(url=url,created_at__gte=yesterday)
+            if (requests.count()>0):
+                response=[]
+                for request in requests:
+                    status_code=request.status_code
+                    status="success" if status_code>=200 and status<300 else "fail"
+                    url=request.url.address
+                    response.append({"status":status,"url":url,"status_code":status_code,"created_at":request.created_at})
+                return Response(response, status=status.HTTP_200_OK)
+            else:
+                response={"message":"No requests found  for this url"}
+                return Response(response, status=status.HTTP_204_NO_CONTENT)
+            
+        else:
+            response={"message":"You are not authorized to view this url"}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 class CallUrlView(APIView):
     permission_classes = (AllowAny,)
     def get(self, request, *args, **kwargs):
