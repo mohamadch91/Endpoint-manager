5a74500e89e4ca447ca48035a6bfc98c947471a1
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-19 11:48:56 +0330

    feat: add Request serializer

--- endpoints/serializers.py
+++ endpoints/serializers.py
@@ -6,7 +6,7 @@ from .models import Url
 class UrlSerializer(serializers.ModelSerializer):
     class Meta:
         model = Url
-        fields = ['pk','url','user','fail_limit','success_count','fail_count','created_at','updated_at']
+        fields = ['id','url','user','fail_limit','success_count','fail_count','created_at','updated_at']
         extra_kwargs = {
             'url': {'required': True},
             'user': {'required': True},
@@ -14,3 +14,12 @@ class UrlSerializer(serializers.ModelSerializer):
         }
         read_only_fields = ['created_at','updated_at']
 
+class RequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Url
+        fields = ['id','url','status_code','created_at','updated_at']
+        extra_kwargs = {
+            'url': {'required': True},
+            'status_code': {'required': True},
+        }
+        read_only_fields = ['created_at','updated_at']
