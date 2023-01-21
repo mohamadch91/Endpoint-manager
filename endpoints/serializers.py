2b22736e39866c77e734853f3fb949f7b9d0d902
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-21 19:54:40 +0330

    fix: remove user from create endpoint

--- endpoints/serializers.py
+++ endpoints/serializers.py
@@ -6,10 +6,9 @@ from .models import Endpoint,Request
 class EndpointSerializer(serializers.ModelSerializer):
     class Meta:
         model = Endpoint
-        fields = ['id','address','user','fail_limit','success_count','fail_count','created_at','updated_at']
+        fields = ['id','address','fail_limit','success_count','fail_count','created_at','updated_at']
         extra_kwargs = {
             'address': {'required': True},
-            'user': {'required': True},
             'fail_limit': {'required': True},
         }
         read_only_fields = ['created_at','updated_at']
