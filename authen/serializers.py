8f7e6619187660d2bb0992cc153288a67539aa8a
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-21 19:52:48 +0330

    feat: change serializer fields order

--- authen/serializers.py
+++ authen/serializers.py
@@ -1,12 +1,8 @@
 
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from .models import User
-
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.hashers import make_password
-
-import json
 
 
 
