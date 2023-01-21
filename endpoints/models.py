18461576a3e4b8fb46cdfe50ed16b7211006a528
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-21 20:24:05 +0330

    fix: typo in Request model field

--- endpoints/models.py
+++ endpoints/models.py
@@ -21,7 +21,7 @@ class Endpoint(models.Model):
         return self.user.username+" : "+self.address
 class Request(models.Model):
     id=models.AutoField(primary_key=True,db_index=True)
-    enpoint=models.ForeignKey(Endpoint,on_delete=models.CASCADE,blank=True,null=True,db_index=True)
+    endpoint=models.ForeignKey(Endpoint,on_delete=models.CASCADE,blank=True,null=True,db_index=True)
     status_code=models.IntegerField(default=0)    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
