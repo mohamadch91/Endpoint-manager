640e44bf42be332c8327d823f834caa2be4d209c
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-26 01:42:26 +0330

    docs: authen/views

--- authen/views.py
+++ authen/views.py
@@ -1,15 +1,5 @@
-import json
-from os import stat
-import os
-from urllib import response
-from django.shortcuts import render
-from django.contrib.auth.hashers import make_password
 
-# Create your views here.
-from .serializers import ChangePasswordSerializer,UpdateUserSerializer
 from rest_framework.permissions import AllowAny
-
-from .models import User
 from .serializers import RegisterSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
@@ -17,14 +7,25 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 
 class RegisterView(generics.CreateAPIView):
+    """view for registering a new user
+
+    Args:
+        generics (Api view): Api view for creating a new user
+    """
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer   
 
 class LogoutView(APIView):
+    """Logout view for logging out a user and blacklisting the refresh token
+
+    Args:
+        APIView (Django view): Django view for logging out a user
+
+    Returns:
+        _type_: Http response
+    """
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
