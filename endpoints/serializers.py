d55cecb8715ed516f227c342b3489f705778b88d
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-26 01:45:29 +0330

    docs: endpoints/serializers

--- endpoints/serializers.py
+++ endpoints/serializers.py
@@ -4,6 +4,11 @@ from rest_framework import serializers
 from .models import Endpoint,Request
 
 class EndpointRegisterSerializer(serializers.ModelSerializer):
+    """Endpoint Register Serializer for creating new endpoints
+
+    Args:
+        serializers (Django serializers): Django serializers
+    """
     class Meta:
         model = Endpoint
         fields = ['id','address','user','fail_limit','created_at','updated_at']
