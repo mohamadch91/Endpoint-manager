938a170bb4bf02411bcdb87a0377b4bc466541d3
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-26 01:52:31 +0330

    docs: endpoints/views

--- endpoints/views.py
+++ endpoints/views.py
@@ -13,17 +13,21 @@ from django.shortcuts import get_object_or_404
 import datetime
 import copy
 class EndpointCreateView(generics.CreateAPIView):
+    #Check for user authentication
     permission_classes = (IsAuthenticated,)
     serializer_class = EndpointSerializer   
-    def post(self, request):
+    def post(self, request: Request, *args, **kwargs)-> Response:
+        #get user from request
         user=get_object_or_404(User,pk=request.user.id)
         user_Endpoint=user.endpoint_count
+        #check if user has reached the limit of 20 endpoints
         if user_Endpoint<20:
             request_data={
                 "user":user.id,
                 "address":request.data['address'],
                 "fail_limit":request.data['fail_limit']
             }
+            #create endpoint
             serializer = EndpointRegisterSerializer(data=request_data)
             if serializer.is_valid():
                 user.endpoint_count+=1
@@ -43,24 +47,32 @@ class EndpointCreateView(generics.CreateAPIView):
         
 
 class UserEndpointView(generics.ListAPIView):
+    #Check for user authentication
     permission_classes = (IsAuthenticated,)
     serializer_class = EndpointSerializer
-    def get_queryset(self):
+    def get_queryset(self)-> Response:
+        #all endpoints of the user
         return Endpoint.objects.filter(user=self.request.user)
 
 class EndpointStatsView(generics.RetrieveAPIView):
+    #Check for user authentication
     permission_classes = (IsAuthenticated,)
     serializer_class = EndpointSerializer
     queryset=Endpoint.objects.all()
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs)-> Response:
+        #get endpoint id from url
         id=kwargs['pk']
-        print(id)
+        #get endpoint
         endpoint=get_object_or_404(Endpoint,id=id)
+        #check if the user is the owner of the endpoint
         if (endpoint.user==self.request.user):
+            #get all requests for this endpoint in the last 24 hours
             yesterday=datetime.datetime.now()-datetime.timedelta(days=1)
             requests=Request.objects.filter(endpoint=endpoint,created_at__gte=yesterday)
+            #check if there are requests
             if (requests.count()>0):
                 response=[]
+                #get the status of each request
                 for request in requests:
                     status_code=request.status_code
                     status_type="success" if status_code>=200 and status_code<300 else "fail"
@@ -68,14 +80,17 @@ class EndpointStatsView(generics.RetrieveAPIView):
                     response.append({"status":status_type,"endpoint":endpoint,"status_code":status_code,"created_at":request.created_at})
                 return Response(response, status=status.HTTP_200_OK)
             else:
+                #if there are no requests
                 response={"message":"No requests found  for this endpoint"}
                 return Response(response, status=status.HTTP_204_NO_CONTENT)
             
         else:
+            #if the user is not the owner of the endpoint
             response={"message":"You are not authorized to view this endpoint"}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
 class CallEndpointView(APIView):
     permission_classes = (AllowAny,)
+    #some junk method to call the endpoint
     def get(self, request, *args, **kwargs):
         endpoint=get_object_or_404(Endpoint,address=self.kwargs['endpoint'])
         endpoint.success_count+=1
@@ -170,17 +185,24 @@ class CallEndpointView(APIView):
          
         
 class EndpointWarningView(generics.ListAPIView):
+    #check if user authenticated
     permission_classes = (IsAuthenticated,)
     queryset = Endpoint.objects.all()
-    def get (self, request, *args, **kwargs):
+    def get (self, request, *args, **kwargs)->Response:
+        #get the endpoint id from the url
         id=self.kwargs['pk']
         endpoint=get_object_or_404(Endpoint,id=id)
+        #check if the user is the owner of the endpoint
         if(endpoint.user == request.user):
+            #check if the endpoint fail limit is exceeded
             if (endpoint.fail_count>endpoint.fail_limit):
+                #get the requests that have status code greater or equal than 300
                 requests=Request.objects.filter(endpoint=endpoint,status_code__gte=300)
+                #get the difference between the fail count and the fail limit
                 diffrence=endpoint.fail_count-endpoint.fail_limit
                 response=[]
                 length=requests.count()
+                #loop through the failed requests and create a response
                 for i in range(diffrence):
                     serializer=RequestSerializer(requests[length-i-1])
                     serilizer_data=copy.deepcopy(serializer.data)
@@ -197,9 +219,11 @@ class EndpointWarningView(generics.ListAPIView):
                 return Response(response, status=status.HTTP_200_OK)
                 
             else:
+                #if the endpoint fail limit is not exceeded return a success response
                 response={"message":"Endpoint is working properly"}
                 return Response(response, status=status.HTTP_200_OK)
         else:
+            #if the user is not the owner of the endpoint return a bad request response
             response={"message":"You are not authorized to view this endpoint"}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
         
