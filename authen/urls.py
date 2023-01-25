60a6a8d491ff76af7a2802bf1ddb8f56f8a51c6d
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-26 01:34:59 +0330

    feat: remove unused views in authen

--- authen/urls.py
+++ authen/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
-    
+    path('logout/', LogoutView.as_view(), name='auth_logout'),
     
 
 
--- authen/views.py
+++ authen/views.py
@@ -23,3 +23,16 @@ from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, Ou
 class RegisterView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer   
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)    
