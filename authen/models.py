0b23dd51b35189daf2f05fd30c1fe156f29bf493
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-26 01:31:59 +0330

    docs: authen/models.py

--- authen/models.py
+++ authen/models.py
@@ -5,7 +5,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-  
+    """User model
+
+    Args:
+        AbstractUser (Django Object): Base user defined in django
+    """
     name = models.CharField(max_length=200, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
