d96f97cecd27db03860715ec25296de79ee0bd46
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-26 01:40:06 +0330

    docs: serializers

--- authen/urls.py
+++ authen/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 from rest_framework_simplejwt.views import   TokenRefreshView, TokenObtainPairView
 
-
+"""define the urls for the authen app
+"""
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
